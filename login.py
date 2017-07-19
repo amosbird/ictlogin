1fb94391ed071c34efb6ca4c557dca279dde7128
Amos Bird <amosbird@gmail.com>
2017-07-19 15:00:11 +0800

    clean up phantomjs.

--- login.py
+++ login.py
@@ -2,8 +2,10 @@
 # -*-coding:utf-8-*-
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import time
 
-driver = webdriver.PhantomJS()
+# driver = webdriver.PhantomJS()
+driver = webdriver.Chrome()
 driver.get('http://159.226.39.22/srun_portal_pc.php?ac_id=1&')
 
 name = driver.find_element_by_name("username")
@@ -11,3 +13,6 @@ name.send_keys('<username>')
 password = driver.find_element_by_id('password')
 password.send_keys('<password>')
 password.send_keys(Keys.RETURN)
+
+time.sleep(1)
+driver.quit()
