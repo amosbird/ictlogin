b3e6d31fd6cb2ecf529069454ed22e7ab9a21235
AveryLiu <averyliu2013@gmail.com>
2018-02-01 15:50:24 +0800

    Updated README

--- login.py
+++ login.py
@@ -12,9 +12,9 @@ def connect():
     driver.get('http://159.226.39.22/srun_portal_pc.php?ac_id=1&')
 
     name = driver.find_element_by_name("username")
-    name.send_keys('username')
+    name.send_keys('YOUR_USERNAME')
     password = driver.find_element_by_id('password')
-    password.send_keys('password')
+    password.send_keys('YOUR_PASSWORD')
     password.send_keys(Keys.RETURN)
 
     logging.info("Connected")
