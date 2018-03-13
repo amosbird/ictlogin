73ab7e0b5fbb2525b57c3df2b01d5db1f6b2700d
Amos Bird <amosbird@gmail.com>
2018-03-13 15:56:34 +0800

    fix login issue.

--- login.py
+++ login.py
@@ -5,13 +5,11 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 driver = webdriver.PhantomJS()
-driver.get('http://159.226.39.22/srun_portal_pc.php?ac_id=1&')
-
+driver.get('https://gw.ict.ac.cn/srun_portal_pc.php?ac_id=1&')
 name = driver.find_element_by_name("username")
 name.send_keys('amos')
 password = driver.find_element_by_id('password')
 password.send_keys('bird')
 password.send_keys(Keys.RETURN)
-
 time.sleep(1)
 driver.quit()
